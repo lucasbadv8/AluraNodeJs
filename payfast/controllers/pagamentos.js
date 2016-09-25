914ff69910ef9ed35d94dc4d687d16f24128bc34
Lucas Lopes <lucasbadv8@gmail.com>
2016-09-24 21:23:48 -0300

    adicionando body-parser

--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -3,4 +3,10 @@ module.exports = function(app) {
         console.log('Recebida a requisicao');
         res.send('OK.');
     });
+
+    app.post('/pagamentos/pagamento', function(req, res) {
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('ok.');
+    })
 }
